1e6427d8e45774af279b29e2bdfb141db29abbde
Alfred Neumayer <dev.beidl@gmail.com>
2019-05-19 13:24:37 +0200

    FileDetailsHelper: Conditionally support theme providers

--- src/filedetailshelper.cpp
+++ src/filedetailshelper.cpp
@@ -6,6 +6,7 @@ FileDetailsHelper::FileDetailsHelper(QObject *parent) : QObject(parent)
 }
 
 QString FileDetailsHelper::getIconFromMime(const QString& mimeType) {
+#ifndef OS_SUPPORTS_THEME_PROVIDER
     if (mimeType.indexOf("image") == 0) {
         return QStringLiteral("qrc:/icons/theme/mimetypes/64/image-x-generic.svg");
     } else if(mimeType.indexOf("audio") == 0) {
@@ -15,6 +16,17 @@ QString FileDetailsHelper::getIconFromMime(const QString& mimeType) {
     } else {
         return QStringLiteral("qrc:/icons/theme/mimetypes/64/text-x-plain.svg");
     }
+#else
+    if (mimeType.indexOf("image") == 0) {
+        return QStringLiteral("image://theme/image-x-generic-symbolic");
+    } else if(mimeType.indexOf("audio") == 0) {
+        return QStringLiteral("image://theme/audio-x-generic-symbolic");
+    } else if(mimeType.indexOf("video") == 0) {
+        return QStringLiteral("image://theme/video-x-generic-symbolic");
+    } else {
+        return QStringLiteral("image://theme/text-x-generic-symbolic");
+    }
+#endif
 }
 
 QString FileDetailsHelper::getHRSize(quint64 bytes) {
